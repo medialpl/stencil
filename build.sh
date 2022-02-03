4fe367921776aee0135d652798e91e71e20479f4
Kamil Dratwa <kamil.dratwa@mytriorings.com>
2022-02-03 08:55:18 -0500

    update build

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 docker build -t stencil_node ./env/node \
   && echo "$(tput setaf 1)Run: $(tput sgr0)npm i && stencil init" \
-  && docker run -it -v $(pwd)/src:/theme -p3000:3000 stencil_node /bin/bash
+  && docker run -it -v $(pwd)/themes/cornerstone:/theme -p3000:3000 stencil_node /bin/bash
