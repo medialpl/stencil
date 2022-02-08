b64f2e11798598603b39b9e9a532c4c23114b8e1
Kamil Dratwa <kamil.dratwa@mytriorings.com>
2022-02-08 06:55:07 -0500

    fix build script

--- build.sh
+++ build.sh
@@ -6,13 +6,13 @@ RUNTIME_CONTAINER=stencil_dev
 THEME="${1}"
 THEME_DIRECTORY="$(pwd)/themes/${THEME}"
 
-if [ -z "${THEME}"]
+if [ -z "${THEME}" ]
   then
     echo "Please provide the theme name."
     exit
 fi
 
-if [ ! -d "${}" ]
+if [ ! -d "${THEME_DIRECTORY}" ]
   then
     echo "Theme directory was not found: ${THEME_DIRECTORY}"
     exit
@@ -21,5 +21,5 @@ fi
 [ "$(docker container ls -a | grep ${SETUP_CONTAINER})" ] \
   && docker image rm ${SETUP_CONTAINER}
 
-docker build -t ${RUNTIME_CONTAINER} ./env/node \
+docker build -t ${RUNTIME_CONTAINER} ./env/node_stencil \
   && docker run -it --name ${SETUP_CONTAINER}  -v ${THEME_DIRECTORY}:/theme -p3000:3000 ${RUNTIME_CONTAINER} init
