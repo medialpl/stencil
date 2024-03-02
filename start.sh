3a2fb294c5a491cd690f85a31335159789a77d12
Kamil Dratwa <kamil.dratwa@gmail.com>
2024-03-02 14:55:08 +0100

    handle channel id in the start script

--- start.sh
+++ start.sh
@@ -1,2 +1,4 @@
-export STENCIL="start -v <theme_version_name>"
+# <theme_version_name> - string containing your theme variation name
+# <channel_id> - number representing sales channel id to use (usually the default channel is 1)
+export STENCIL="start -v <theme_variation_name> -c <channel_id>"
 docker-compose up
